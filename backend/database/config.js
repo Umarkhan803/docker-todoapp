dd131b62a8b4da7ba12e1e596d887d18f0746f69
Umarkhan803 <umar.khan@quantumquest.in>
2026-02-17 15:19:42 +0530

    mongo db reslove

--- backend/database/config.js
+++ backend/database/config.js
@@ -3,9 +3,8 @@ const dotenv = require("dotenv");
 dotenv.config();
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(
-      "mongodb+srv://mohammedumarkhan803_db_user:5mERZln22zgLZ51J@todoapp.jue0hzv.mongodb.net/",
-    );
+    const conn = await mongoose.connect(process.env.MONGODB_URI);
+
     console.log(`MongoDB Connected`);
   } catch (error) {
     console.error(`Error: ${error.message}`);
